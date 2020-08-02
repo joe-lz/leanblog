7ab6f38ae5b5e05c764603ab0d1e5e93e09f7599
jeodiong <jeodiong@hotmail.com>
2020-08-02 15:57:04 +0800

    feat:  menu

--- src/lib/defaultMenu.js
+++ src/lib/defaultMenu.js
@@ -26,11 +26,11 @@ export default [
 
     children: [
       {
-        title: "快应用",
+        title: "交互设计",
         key: uuidv4(),
       },
       {
-        title: "小程序",
+        title: "UI设计",
         key: uuidv4(),
       },
     ],
@@ -38,16 +38,63 @@ export default [
   {
     title: "产品",
     key: uuidv4(),
-
-    children: [
-      {
-        title: "快应用",
-        key: uuidv4(),
-      },
-      {
-        title: "小程序",
-        key: uuidv4(),
-      },
-    ],
   },
 ];
+
+// 拖拽更新目录
+export const handleformatData = (info, curMenu) => {
+  const dropKey = info.node.props.eventKey;
+  const dragKey = info.dragNode.props.eventKey;
+  const dropPos = info.node.props.pos.split("-");
+  const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
+
+  const loop = (data, key, callback) => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].key === key) {
+        return callback(data[i], i, data);
+      }
+      if (data[i].children) {
+        loop(data[i].children, key, callback);
+      }
+    }
+  };
+  const data = [...curMenu];
+  // Find dragObject
+  let dragObj;
+  loop(data, dragKey, (item, index, arr) => {
+    arr.splice(index, 1);
+    dragObj = item;
+  });
+
+  if (!info.dropToGap) {
+    // Drop on the content
+    loop(data, dropKey, (item) => {
+      item.children = item.children || [];
+      // where to insert 示例添加到尾部，可以是随意位置
+      item.children.push(dragObj);
+    });
+  } else if (
+    (info.node.props.children || []).length > 0 && // Has children
+    info.node.props.expanded && // Is expanded
+    dropPosition === 1 // On the bottom gap
+  ) {
+    loop(data, dropKey, (item) => {
+      item.children = item.children || [];
+      // where to insert 示例添加到头部，可以是随意位置
+      item.children.unshift(dragObj);
+    });
+  } else {
+    let ar;
+    let i;
+    loop(data, dropKey, (item, index, arr) => {
+      ar = arr;
+      i = index;
+    });
+    if (dropPosition === -1) {
+      ar.splice(i, 0, dragObj);
+    } else {
+      ar.splice(i + 1, 0, dragObj);
+    }
+  }
+  return data;
+};
--- src/pages/_user/feature/menus.js
+++ src/pages/_user/feature/menus.js
@@ -1,19 +1,28 @@
 import Head from "next/head";
 import AV from "leancloud-storage";
 import dynamic from "next/dynamic";
-import { Tree, Button } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { Tree, Button, notification, Input, Modal } from "antd";
+import {
+  PlusOutlined,
+  DownOutlined,
+  InfoCircleOutlined,
+} from "@ant-design/icons";
 import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import styles from "./index.module.scss";
 import Layout from "src/components/_user/Layout";
-import defaultMenu from "src/lib/defaultMenu";
+import defaultMenu, { handleformatData } from "src/lib/defaultMenu";
 
 function AdminHome() {
+  const [curMenuObj, setcurMenuObj] = useState(null);
   const [curMenu, setcurMenu] = useState([]);
+  const [level1Show, setlevel1Show] = useState(false);
+  const [level1Title, setlevel1Title] = useState("");
+  const [selectedMenu, setselectedMenu] = useState(null);
+  const [updateShow, setupdateShow] = useState(false);
 
-  useEffect(() => {
-    // 获取菜单
+  const getMenuList = () => {
     const query = new AV.Query("Menus");
     query.descending("createdAt");
     query
@@ -21,6 +30,7 @@ function AdminHome() {
       .then((res) => {
         if (res) {
           setcurMenu(res.attributes.value);
+          setcurMenuObj(res);
         } else {
           // 创建默认菜单
           const menusObject = new AV.Object("Menus");
@@ -28,8 +38,10 @@ function AdminHome() {
           // 将对象保存到云端
           menusObject.save().then(
             (todo) => {
-              setcurMenu(defaultMenu);
+              console.log(todo);
               // 成功保存之后，执行其他逻辑
+              setcurMenu(defaultMenu);
+              setcurMenuObj(todo);
             },
             (error) => {
               // 异常处理
@@ -40,26 +52,108 @@ function AdminHome() {
       .catch((err) => {
         console.log({ err });
       });
+  };
+
+  const onSelect = (select, info) => {
+    const obj = {
+      title: info.node.title,
+      key: info.node.key,
+    };
+    setselectedMenu(obj);
+    setupdateShow(true);
+  };
+
+  const updateFunction = (newValue) => {
+    if (curMenuObj) {
+      curMenuObj.set("value", newValue);
+      curMenuObj.save().then(() => {
+        setupdateShow(false);
+        getMenuList();
+        notification.success({
+          message: "更新成功",
+          // description: "请输入用户名、密码",
+        });
+      });
+    }
+  };
+
+  const onDrop = (info) => {
+    const result = handleformatData(info, curMenu);
+    updateFunction(result);
+  };
+
+  const createLevel1 = () => {
+    if (!level1Title) {
+      notification.error({
+        message: "请输入标题",
+        // description: "请输入用户名、密码",
+      });
+      return;
+    }
+    const newObj = {
+      title: level1Title,
+      key: uuidv4(),
+    };
+    curMenu.push(newObj);
+    curMenuObj.set("value", curMenu);
+    // 将对象保存到云端
+    curMenuObj.save().then(
+      () => {
+        setlevel1Show(false);
+        getMenuList();
+        notification.success({
+          message: "更新成功",
+          // description: "请输入用户名、密码",
+        });
+      },
+      (error) => {
+        // 异常处理
+      }
+    );
+  };
+
+  const handleUpdate = () => {
+    const newcurMenu = curMenu.map((obj1) => {
+      if (obj1.key === selectedMenu.key) {
+        obj1.title = selectedMenu.title;
+      }
+
+      if (obj1.children) {
+        obj1.children.map((obj2) => {
+          if (obj2.key === selectedMenu.key) {
+            obj2.title = selectedMenu.title;
+          }
+          return obj2;
+        });
+      }
+      return obj1;
+    });
+    updateFunction(newcurMenu);
+  };
+
+  useEffect(() => {
+    // 获取菜单
+    getMenuList();
   }, []);
-
-  const onSelect = (selectedKeys, info) => {
-    console.log("selected", selectedKeys, info);
-  };
-
-  const onCheck = (checkedKeys, info) => {
-    console.log("onCheck", checkedKeys, info);
-  };
 
   return (
     <Layout>
       <p className="_admin_body_section_title">自定义菜单</p>
-
       <div className={styles.block} style={{ padding: 30 }}>
+        <div className={styles.block_waring}>
+          <InfoCircleOutlined
+            style={{ color: "RGBA(53, 90, 207, 1.00)", fontSize: 20 }}
+          />
+          <p>拖拽目录进行排序，和层级管理</p>
+        </div>
         <Tree
+          showLine
+          // switcherIcon={<DownOutlined />}
           defaultExpandAll
           onSelect={onSelect}
+          onDrop={onDrop}
           draggable
-          blockNode
+          // blockNode
           treeData={curMenu}
         />
       </div>
@@ -70,10 +164,13 @@ function AdminHome() {
           icon={<PlusOutlined />}
           size={14}
           style={{ marginRight: 15 }}
+          onClick={() => {
+            setlevel1Show(true);
+          }}
         >
           一级目录
         </Button>
-        <Button
+        {/* <Button
           type="primary"
           shape="round"
           icon={<PlusOutlined />}
@@ -81,8 +178,46 @@ function AdminHome() {
           style={{ marginRight: 15 }}
         >
           二级目录
-        </Button>
+        </Button> */}
       </div>
+      <Modal
+        title="一级目录"
+        visible={level1Show}
+        onCancel={() => {
+          setlevel1Show(false);
+        }}
+        onOk={() => {
+          createLevel1();
+        }}
+      >
+        <Input
+          placeholder="标题"
+          onChange={(e) => {
+            setlevel1Title(e.target.value);
+          }}
+        />
+      </Modal>
+      <Modal
+        title="更新"
+        visible={updateShow}
+        onCancel={() => {
+          setupdateShow(false);
+        }}
+        onOk={() => {
+          handleUpdate();
+        }}
+      >
+        <Input
+          placeholder="标题"
+          value={selectedMenu ? selectedMenu.title : ""}
+          onChange={(e) => {
+            setselectedMenu({
+              title: e.target.value,
+              key: selectedMenu.key,
+            });
+          }}
+        />
+      </Modal>
     </Layout>
   );
 }
