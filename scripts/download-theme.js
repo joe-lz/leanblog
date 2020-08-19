f6016313d0c1033e0393d558608f696a1762322b
bangdong.chen <bangdong.chen@crgrcent.com>
2020-08-19 10:59:15 +0800

    ci:  ci配置更新

--- scripts/download-theme.js
+++ scripts/download-theme.js
@@ -7,10 +7,10 @@ download('joe-lz/ombTheme-white-admin', 'temp_theme/admin', async (err) => {
   await copyFolder({ oldPath: 'temp_theme/admin/pages', newPath: 'src/pages/admin' })
   await copyFolder({ oldPath: 'temp_theme/admin/components', newPath: 'src/components/admin' })
 
-  // download('joe-lz/ombTheme-white-www', 'temp_theme/www', async (err) {
-  //   if (!err) {
-  //   }
-  // })
+  download('joe-lz/ombTheme-white-www', 'temp_theme/www', async (err) => {
+    await copyFolder({ oldPath: 'temp_theme/www/pages', newPath: 'src/pages/www' })
+    await copyFolder({ oldPath: 'temp_theme/www/components', newPath: 'src/components/www' })
+  })
 })
 
 const copyFolder = ({ oldPath, newPath }) => {
