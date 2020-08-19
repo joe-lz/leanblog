c2140deae2f87b71ea61ecd0d236c159e02557af
bangdong.chen <bangdong.chen@crgrcent.com>
2020-08-19 11:05:42 +0800

    ci:  ci配置更新

--- scripts/replace-gitignore/index.js
+++ scripts/replace-gitignore/index.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const rimraf = require('rimraf')
 
 rimraf.sync('.gitignore')
-fs.rename('scripts/replace-gitignore/template.gitignore', '.gitignore', (err) => {
+fs.rename('scripts/replace-gitignore/.gitignore.template', '.gitignore', (err) => {
   console.log(err);
 })
