54e302916b00eb8a704392d6046a2a9fc98e5354
bangdong.chen <bangdong.chen@crgrcent.com>
2020-08-19 10:39:28 +0800

    feat:  remove componnents

--- scripts/download-theme.js
+++ scripts/download-theme.js
@@ -1,8 +1,27 @@
-const config = require('../omb.config.js')
 const download = require('download-git-repo')
+const fs = require('fs')
+const rimraf = require('rimraf')
+const config = require('../omb.config.js')
 
-download('joe-lz/ombTheme-white-www', 'src/pages/www', function (err) {
-  if (!err) {
-    download('joe-lz/ombTheme-white-admin', 'src/pages/admin', function (err) {})
-  }
+download('joe-lz/ombTheme-white-admin', 'temp_theme/admin', async (err) => {
+  await copyFolder({ oldPath: 'temp_theme/admin/pages', newPath: 'src/pages/admin' })
+  await copyFolder({ oldPath: 'temp_theme/admin/components', newPath: 'src/components/admin' })
+
+  // download('joe-lz/ombTheme-white-www', 'temp_theme/www', async (err) {
+  //   if (!err) {
+  //   }
+  // })
 })
+
+const copyFolder = ({ oldPath, newPath }) => {
+  return new Promise((resolve, reject) => {
+    rimraf.sync(newPath)
+    fs.rename(oldPath, newPath, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
